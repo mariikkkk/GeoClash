5b609378e69c6b6751622eae0fa1f93e53bec63b
mariikkkk <maratts12342334@gmail.com>
2025-11-11 23:33:14 +0300

    Удалил чуть ненужных файлов, добавил диаграммы и READMDE файл

--- scene/Manager/upgrade_manager.gd
+++ scene/Manager/upgrade_manager.gd
@@ -1,21 +1,21 @@
 extends Node
 
-@export var exp_manager: ExperienceManager
-@export var upgrade_pool: Array[AbilityUpgrade]
-var current_upgrades = {}
-
-func _ready():
-	exp_manager.lvl_up.connect(on_lvl_up)
-	
-func on_lvl_up(current_level):
-	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if chosen_upgrade == null:
-		return
-	var has_upgrade = current_upgrades.has(chosen_upgrade.id)
-	if !has_upgrade:
-		current_upgrades[chosen_upgrade.id] = {
-			"upgrade": chosen_upgrade,
-			"quantity": 1 #количество апргрейдов
-		}
-	else:
-		current_upgrades[chosen_upgrade.id]["quantity"] += 1
+#@export var exp_manager: ExperienceManager
+#@export var upgrade_pool: Array[AbilityUpgrade]
+#@export var upgrade_ui
+#
+#func _ready():
+	#exp_manager.lvl_up.connect(on_lvl_up)
+	#
+#func on_lvl_up(current_level):
+	#var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
+	#if chosen_upgrade == null:
+		#return
+	#var has_upgrade = current_upgrades.has(chosen_upgrade.id)
+	#if !has_upgrade:
+		#current_upgrades[chosen_upgrade.id] = {
+			#"upgrade": chosen_upgrade,
+			#"quantity": 1 #количество апргрейдов
+		#}
+	#else:
+		#current_upgrades[chosen_upgrade.id]["quantity"] += 1
